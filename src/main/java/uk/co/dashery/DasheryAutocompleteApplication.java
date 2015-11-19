c0981b4d13c6e8e28050c7dd3eb7989a2911335c
Angus Goldsmith <angus.j.goldsmith@gmail.com>
2015-11-19 20:46:32 +0000

    Add tokens method for typeahead to easily consume from.

--- src/main/java/uk/co/dashery/DasheryAutocompleteApplication.java
+++ src/main/java/uk/co/dashery/DasheryAutocompleteApplication.java
@@ -1,31 +1,29 @@
 package uk.co.dashery;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 @SpringBootApplication
+@RestController
 public class DasheryAutocompleteApplication {
+    @Autowired
+    private TokenRepository tokenRepository;
 
-    /**
-     * http://stackoverflow.com/questions/31724994/spring-data-rest-and-cors/31748398#31748398
-     */
-    @Bean
-    public CorsFilter corsFilter() {
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); // you USUALLY want this
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+    @CrossOrigin
+    @RequestMapping("/tokens")
+    public List<Token> tokens() {
+        return tokenRepository.findAll();
     }
 
     public static void main(String[] args) {
