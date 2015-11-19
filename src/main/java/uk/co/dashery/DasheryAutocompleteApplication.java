0775957872c38dc6e707d0987034f4864cb05a7c
Angus Goldsmith <angus.j.goldsmith@gmail.com>
2015-11-19 20:26:34 +0000

    Added CORS filter.

--- src/main/java/uk/co/dashery/DasheryAutocompleteApplication.java
+++ src/main/java/uk/co/dashery/DasheryAutocompleteApplication.java
@@ -2,13 +2,33 @@ package uk.co.dashery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class DasheryAutocompleteApplication {
 
+    /**
+     * http://stackoverflow.com/questions/31724994/spring-data-rest-and-cors/31748398#31748398
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // you USUALLY want this
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DasheryAutocompleteApplication.class, args);
     }
-
 }
