f08e949d240c640e78f4e9443c3a26026811ff30
Angus Goldsmith <angus.j.goldsmith@gmail.com>
2015-12-22 20:04:21 +0000

    Made token creation async.

--- src/test/java/uk/co/dashery/TestDasheryAutocompleteConfig.java
+++ src/test/java/uk/co/dashery/TestDasheryAutocompleteConfig.java
@@ -2,10 +2,12 @@ package uk.co.dashery;
 
 import com.github.fakemongo.Fongo;
 import com.mongodb.Mongo;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
-@SpringBootApplication
+@Configuration
+@Import(DasheryAutocompleteApplication.class)
 public class TestDasheryAutocompleteConfig extends AbstractMongoConfiguration {
     @Override
     protected String getDatabaseName() {
